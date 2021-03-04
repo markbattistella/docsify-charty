bfe0b331aaa2a6544321d39186ada7fdd145270c
Mark Battistella <markbattistella@users.noreply.github.com>
2021-03-04 17:27:41 +1100

    finished: bars and section

--- docs/docsify-charty.js
+++ docs/docsify-charty.js
@@ -15,6 +15,18 @@ function getChartyOptions( chartyOptions ) {
 	let theme	= chartyOptions.theme ?
 					chartyOptions.theme : '#0984e3';
 
+
+// "title"
+// "config"
+// "type"
+// "color"
+// "labels"
+// "numbers"
+// "data"
+
+
+
+
 	// build the array
 	var outputArray = [
 		theme
@@ -35,60 +47,79 @@ function charty( hook, vm ) {
 	// get the variables from the cofig
 	const	chartyOptionsArray = getChartyOptions( chartyOptions ),
 
-			// create them easier to read
+			// create global options
 			arrayID		= chartyOptionsArray[0];
 
 
-	// docsify: when script starts running
-	// hook.init(function() {
-	// });
+
+
+	// find the arc co-ordinates
+	// TODO: change percent to variable
+	function getCoordinatesForPercent( value ) {
+		const	x = Math.cos(2 * Math.PI * value),
+				y = Math.sin(2 * Math.PI * value);
+
+		return [x, y];
+	}
 
 
 
-
-
-
-
-	hook.afterEach(function (html, next) {
+	//
+	// MARK: - after the markdown has been parsed
+	//
+	hook.afterEach(function( html, next ) {
 
 		// we load the HTML inside a DOM node to allow for manipulation
 		var htmlElement = document.createElement('div');
 
+		// insert the html into the new element
 		htmlElement.innerHTML = html;
 
+		// find all the charty <pre> tags
 		htmlElement.querySelectorAll('pre[data-lang=charty]').forEach(
+
+			// what to do
 			function( element ) {
 
-				// create a <div class="charty"> to replace the <pre>
-				let replacement		= document.createElement('div'),
-					chartyBars		= '',
-					chartyHeader	= '',
-					charty			= '',
+				// create the variables
 
-					dataColor, dataLabel, dataNumber;
+					// <div> to replace the <pre>
+				let replacement		= document.createElement('div'),
+
+					// blank the rest of the options
+					charty			= '', // full container
+					chartyHeader	= '', // header of chart
+					chartyData		= '', // the inner data
+
+					// data customisations
+					dataColor,
+					dataLabel,
+					dataNumber,
+					dataSize;
 
 				// add in the contents
 				replacement.textContent = element.textContent;
 
 				// create the json data
 				const	jsonData		= JSON.parse( replacement.innerHTML ),
-						array			= jsonData.data,
+						jsonConfig		= jsonData.config,
+						dataArray		= jsonData.data,
 
-						chartyType		= jsonData.config.type ?
-											'charty-' + jsonData.config.type :
-											null,
+						chartyType		=	jsonConfig.type ?
+												`charty-${jsonConfig.type}` :
+												null,
 
-						chartyColor		= jsonData.config.color ?
-											jsonData.config.color :
-											'',
+						chartyColor		=	jsonConfig.color ?
+												jsonConfig.color :
+												'',
 
-						chartyLabel		= jsonData.config.labels ?
-											jsonData.config.labels :
-											true,
+						chartyLabel		=	jsonConfig.labels ?
+												jsonConfig.labels :
+												true,
 
-						chartyNumbers	= jsonData.config.numbers ?
-											jsonData.config.numbers :
-											false;
+						chartyNumbers	=	jsonConfig.numbers ?
+												jsonConfig.numbers :
+												false;
 
 				// add the class
 				replacement.classList.add( 'charty' );
@@ -96,37 +127,134 @@ function charty( hook, vm ) {
 				// set the type attribute
 				replacement.setAttribute( 'type', chartyType );
 
-				// switch the type
+
+				//
+				// MARK: - switch the type
+				//
 				switch( chartyType ) {
-					case 'line':
+
+
+					// normal pie chart via svg
+					case 'charty-pie' :
 						break;
 
-					case 'charty-bar':
-						// loop through the data for output
-						for( let index of array ) {
 
-							// config: top numbers
-							dataColor = chartyColor ?
-											'background:' + chartyColor + ';' :
+
+					// pie chart with middle out
+					case 'charty-donut' :
+						break;
+
+
+
+					// pie chart not whole
+					case 'charty-section' :
+
+						// create some variables
+						let totalPercent = 0;
+
+						const	xmlns	= 'http://www.w3.org/2000/svg',
+								svg		= document.createElementNS(
+											xmlns,
+											'svg'
+										);
+
+						// attribute for sizing based off css
+						svg.setAttributeNS( null, 'viewBox', '-1 -1 2 2' );
+						svg.style.transform = 'rotate(-90deg)';
+
+
+						// loop through each data object
+						dataArray.forEach( data => {
+
+							// destructuring assignment
+							// -- sets the two variables at once
+							const [startX, startY] = getCoordinatesForPercent(
+														totalPercent
+													);
+
+							// each slice starts where the last slice ended
+							// -- so keep a cumulative percent
+							totalPercent += data.value;
+
+							const [endX, endY] =	getCoordinatesForPercent(
+														totalPercent
+													);
+
+							// if the slice is more than 50%
+							// take the large arc (the long way around)
+							const largeArcFlag = data.value > 0.5 ? 1 : 0;
+
+							// create an array
+							// -- join it just for code readability
+							const pathData = [
+								// Move
+								`M ${startX} ${startY}`,
+
+								// Arc
+								`A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`,
+
+								//  Line
+								`L 0 0`,
+							].join(' ');
+
+							// create a path
+							const path = document.createElementNS(
+											xmlns,
+											'path'
+										);
+
+							// add the attributes
+							path.setAttribute( 'd', pathData );
+							path.setAttribute( 'fill', data.color );
+
+							// append it to the svg element
+							svg.appendChild( path );
+						});
+
+						// check if total percent is more than 100%
+						charty =	( totalPercent <= 1 ?
+										svg.outerHTML :
+										null
+									);
+
+						break;
+
+
+
+					// bar chart
+					case 'charty-bar' :
+
+						// loop through each data object
+						dataArray.forEach( data => {
+
+							// config: colour - global
+							dataColor	= chartyColor ?
+											data.color ?
+												`background:${data.color};`  :
+												`background:${chartyColor};` :
 											'';
 
 							// config: base label
-							dataLabel = chartyLabel ?
-											'data-label="' + index.label + '"' :
+							dataLabel	= chartyLabel ?
+											`data-label="${data.label}"`	:
 											'';
 
 							// config: top numbers
-							dataNumber = chartyNumbers ?
-										'data-number="' + index.value + '"' : '';
+							dataNumber	= chartyNumbers ?
+											`data-number="${data.value}"`	:
+											'';
 
-							// loop through all the data
-							// -- build the bars
-							chartyBars +=	'<div class="bar" \
-												style="grid-row-start: calc(100 - ' + index.value + ');' +
-												dataColor + '"' +
-												dataLabel + dataNumber +
-												'></div>';
-						}
+							// config: bar height
+							dataSize 	= `grid-row-start: calc( 100 - ${data.value} );`
+
+							// build the data
+							chartyData += `<div class="data" style="${dataSize} ${dataColor}" ${dataLabel} ${dataNumber}></div>`;
+						});
+
+
+						// assembly
+						charty =	`<div class="data-set">${chartyData}</div>`;
+
 						break;
 
 					// exit if not matched
@@ -134,23 +262,32 @@ function charty( hook, vm ) {
 						return;
 				}
 
-				// add the header if present
-				chartyHeader = jsonData.title === "" || !jsonData.title ?
-									chartyHeader :
-									'<h3>' + jsonData.title + '</h3>';
 
-				//
-				charty = chartyHeader + '<div class="bars">' + chartyBars + '</div>';
+				// check before changing DOM
+				if( charty ) {
 
-				// add in the bars
-				replacement.innerHTML = charty;
+					// add the header (if present)
+					chartyHeader =	jsonData.title == ""	||
+									!jsonData.title			?
+										chartyHeader		:
+										`<h3>${jsonData.title}</h3>`;
 
-				// commit the manipulation
-				element.parentNode.replaceChild(replacement, element);
 
+					// add in the bars
+					replacement.innerHTML = `${chartyHeader} ${charty}`;
+
+					// commit the manipulation
+					element.parentNode.replaceChild(replacement, element);
+
+				} else {
+
+					// exit if no changes
+					return;
+				}
 			}
 		);
 
+		// docsify return data
 		next(htmlElement.innerHTML);
 
 	});
--- docs/site/test.md
+++ docs/site/test.md
@@ -1,74 +1,51 @@
 # Test
 
+ <!-- <svg viewBox="0 0 500 100" style="width: 500px; height: 100px; padding: 20px 20px 20px 0; border-left: 1px dotted #555; border-bottom: 1px dotted #555;">
+  <polyline
+     fill="none"
+     stroke="#0074d9"
+     stroke-width="2"
+     points="
+       00,120
+       20,60
+       40,80
+       60,20
+       80,80
+       100,80
+       120,60
+       140,100
+       160,90
+       180,80
+       200, 110
+       220, 10
+       240, 70
+       260, 100
+       280, 100
+       300, 40
+       320, 0
+       340, 100
+       360, 100
+       380, 120
+       400, 60
+       420, 70
+       440, 80"
+   />
+</svg> -->
+
+
 ```charty
 {
     "title":  "Unit growth",
     "config": {
-        "type":    "bar",
+        "type":    "section",
         "color":   "var(--mb-colour-blue)",
-		"labels":  true,
-		"numbers": false
-    },
-
-    "data": [
-        { "label": "2010", "value": 1 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 54 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 85 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 85 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 100 },
-    	{ "label": "entry1", "value": 65 },
-    	{ "label": "entry1", "value": 100 },
-	    { "label": "entry1", "value": 100 },
-        { "label": "entry2", "value": 65  }
-    ]
-}
-```
-
-Line breaker
-
-```charty
-{
-	"config": {
-        "color":   "var(--mb-colour-yellow)",
 		"labels":  true,
 		"numbers": true
     },
- "data": [
-    { "label": "entry4", "value": 52 },
-    { "label": "entry5", "value": 23  },
-    { "label": "entry7", "value": 67  }
-  ]
+
+    "data": [
+        { "label": "2010",   "value": 0.01, "color": "green" },
+	    { "label": "entry1", "value": 0.99, "color": "var(--mb-colour-yellow)" }
+    ]
 }
 ```
-
-
-
-
-
-
-
-
-
-<!--
-<div class="charty" type="charty-bar">
-	<h3>Unit growth</h3>
-	<div class="bars">
-		<div class="bar" data-value="0" style="grid-row-start: 100;"></div>
-		<div class="bar" data-value="10" style="grid-row-start: 90;"></div>
-		<div class="bar" data-value="20" style="grid-row-start: 80;"></div>
-		<div class="bar" data-value="30" style="grid-row-start: 70;"></div>
-		<div class="bar" data-value="40" style="grid-row-start: 60;"></div>
-		<div class="bar" data-value="50" style="grid-row-start: 50;"></div>
-		<div class="bar" data-value="60" style="grid-row-start: 40;"></div>
-		<div class="bar" data-value="70" style="grid-row-start: 30;"></div>
-		<div class="bar" data-value="80" style="grid-row-start: 20;"></div>
-		<div class="bar" data-value="90" style="grid-row-start: 10;"></div>
-		<div class="bar" data-value="100" style="grid-row-start: 0;"></div>
-	</div>
-</div> -->
